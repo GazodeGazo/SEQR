3440a314ad1c77b9f50ec0bf055e2c309616755c
roroadev <derviguillaume@gmail.com>
2026-01-26 01:12:22 +0100

    Reset site to pre-launch

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -1,12 +1,12 @@
 // SEQR Protocol - Contract Configuration
-// Auto-updated by launcher
+// Pre-launch mode
 
 export const CONTRACTS = {
-  TOKEN: "0x85D9B9bDe17C0dFB13084E4B64ee40526161F8c5",
-  SWAP_HELPER: "0xF9a00A73E471925998D6Bd7F6e9100a170AcC14B",
-  IS_LIVE: true,
-  NAME: "polo",
-  SYMBOL: "POLO",
+  TOKEN: null,
+  SWAP_HELPER: null,
+  IS_LIVE: false,
+  NAME: "SEQR",
+  SYMBOL: "SEQR",
   DECIMALS: 18,
   TOTAL_SUPPLY: "1000000000",
   CHAIN_ID: 8453,
@@ -14,7 +14,7 @@ export const CONTRACTS = {
   RPC_URL: "https://mainnet.base.org",
   BLOCK_EXPLORER: "https://basescan.org",
   TWITTER: "https://x.com/seqrbase",
-  DEXSCREENER: "https://dexscreener.com/base/0x85D9B9bDe17C0dFB13084E4B64ee40526161F8c5",
-  BASESCAN_TOKEN: "https://basescan.org/token/0x85D9B9bDe17C0dFB13084E4B64ee40526161F8c5",
+  DEXSCREENER: null,
+  BASESCAN_TOKEN: null,
 };
 export const isProtocolLive = () => CONTRACTS.IS_LIVE && CONTRACTS.TOKEN !== null;
